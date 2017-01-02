960549205169e76fa7956722b338b48b6ff9a55c
raphaelauv <raphaelauv@users.noreply.github.com>
2017-01-02 02:27:33 +0100

    Ajout MakeFile et Script , nettoyage repertoire SFML des exemepls inutiles

---
+++ Makefile
@@ -0,0 +1,69 @@
+CC=g++ -std=c++11
+CFLAGS=-std=c++0x -I/src/SFML-2.4.1/include
+LDFLAGS=-lsfml-graphics -lsfml-system -lsfml-window
+EXEC= mainTakinCHAR mainTakinINT mainSokobanManual mainSokobanFile main2048Plugin main2048
+
+FRAMWORK=src/CaseGeneric.cpp  src/CaseGeneric.hpp    src/DoublePointer.hpp  src/FramPlateauLand.hpp    src/Display/display.hpp
+SOKOBAN=   src/Sokoban/CaseSokoban.cpp    src/Sokoban/PlateauSokoban.cpp 
+2048= src/2048/Case2048Destroy/Case2048Destroy.hpp  src/2048/Case2048Destroy/Case2048Destroy.cpp  src/2048/Case2048.hpp  src/2048/Plateau2048Plugin.hpp  src/2048/Plateau2048.cpp  src/2048/Case2048.cpp  src/2048/Plateau2048.hpp  src/2048/case2048Negative/Case2048Negative.hpp  src/2048/case2048Negative/Case2048Negative.cpp  src/2048/Plateau2048Plugin.cpp
+
+SRC= $(FRAMWORK)
+
+SRC2= $(FRAMWORK) $(SOKOBAN)
+
+SRC3= $(FRAMWORK) $(2048)
+
+OBJFram= $(SRC:.cpp=.o)
+
+OBJFramSokoban= $(SRC2:.cpp=.o)
+
+OBJFram2048= $(SRC3:.cpp=.o)
+
+all: $(SRC) $(EXEC)
+
+object/main2048.o: src/2048/main2048.cpp
+	$(CC) -o $@ -c $< $(CFLAGS)
+
+main2048: object/main2048.o $(OBJFram2048)
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+object/main2048Plugin.o: src/2048/main2048Plugin.cpp
+	$(CC) -o $@ -c $< $(CFLAGS)
+
+main2048Plugin: object/main2048Plugin.o $(OBJFram2048)
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+object/mainSokobanFile.o: src/Sokoban/mainSokobanFile.cpp
+	$(CC) -o $@ -c $< $(CFLAGS)
+
+mainSokobanFile: object/mainSokobanFile.o $(OBJFramSokoban)
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+object/mainSokobanManual.o: src/Sokoban/mainSokobanManual.cpp
+	$(CC) -o $@ -c $< $(CFLAGS)
+
+mainSokobanManual: object/mainSokobanManual.o $(OBJFramSokoban)
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+object/mainTakinINT.o: src/Takin/mainTakinINT.cpp
+	$(CC) -o $@ -c $< $(CFLAGS)
+
+mainTakinINT: object/mainTakinINT.o $(OBJFram)
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+object/mainTakinCHAR.o: src/Takin/mainTakinCHAR.cpp
+	$(CC) -o $@ -c $< $(CFLAGS)
+
+mainTakinCHAR: object/mainTakinCHAR.o $(OBJFram)
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+
+.cpp.o:
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -rf object/*.o
+	find . -name '*.o' -delete
+
+mrproper: clean
+	rm -rf $(EXEC)
---
+++ makeScript.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+g++ -std=c++0x  -O0 -g3 -c -o "object/DoublePointer.o" "src/DoublePointer.hpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/FramPlateauLand.o" "src/FramPlateauLand.hpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/CaseGeneric.o"  "src/CaseGeneric.cpp"
+
+g++ -std=c++0x -I/src/SFML-2.4.1/include -O0 -g3 -c -o"object/display.o"  "src/Display/display.hpp"
+
+
+
+#g++ -std=c++0x  -O0 -g3 -c -o"object/CaseTakin.o"   "src/Takin/CaseTakin.hpp"
+
+#g++ -std=c++0x  -O0 -g3 -c -o"object/PlateauTakin.o"  "src/Takin/PlateauTakin.hpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/mainTakinINT.o"  "src/Takin/mainTakinINT.cpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/mainTakinCHAR.o"  "src/Takin/mainTakinCHAR.cpp"
+
+
+
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/CaseSokoban.o"   "src/Sokoban/CaseSokoban.cpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/PlateauSokoban.o"  "src/Sokoban/PlateauSokoban.cpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/mainSokobanFile.o"   "src/Sokoban/mainSokobanFile.cpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/mainSokobanManual.o"   "src/Sokoban/mainSokobanManual.cpp"
+
+
+
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/Case2048Negative.o"   "src/2048/case2048Negative/Case2048Negative.cpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/Case2048Destroy.o"  "src/2048/Case2048Destroy/Case2048Destroy.cpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/Plateau2048Plugin.o"  "src/2048/Plateau2048Plugin.cpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/main2048Plugin.o"   "src/2048/main2048Plugin.cpp"
+
+
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/Case2048.o"  "src/2048/Case2048.cpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/Plateau2048.o"   "src/2048/Plateau2048.cpp"
+
+g++ -std=c++0x  -O0 -g3 -c -o"object/main2048.o"   "src/2048/main2048.cpp"
+
+
+
+
+g++  -o "main2048Plugin" object/Case2048.o object/Case2048Negative.o  object/Case2048Destroy.o object/Plateau2048.o object/Plateau2048Plugin.o object/CaseGeneric.o object/main2048Plugin.o   -lsfml-graphics -lsfml-system -lsfml-window
+
+g++  -o "main2048" object/Case2048.o object/Plateau2048.o object/CaseGeneric.o object/main2048.o   -lsfml-graphics -lsfml-system -lsfml-window
+
+g++  -o "mainSokobanManual" object/PlateauSokoban.o object/CaseGeneric.o object/mainSokobanManual.o   -lsfml-graphics -lsfml-system -lsfml-window
+
+g++  -o "mainSokobanFile"  object/PlateauSokoban.o object/CaseGeneric.o object/mainSokobanFile.o   -lsfml-graphics -lsfml-system -lsfml-window
+
+g++  -o "mainTakinINT" object/CaseGeneric.o object/mainTakinINT.o   -lsfml-graphics -lsfml-system -lsfml-window
+
+g++  -o "mainTakinCHAR" object/CaseGeneric.o object/mainTakinCHAR.o
